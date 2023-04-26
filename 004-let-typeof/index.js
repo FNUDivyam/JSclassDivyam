576cd817450d95413578dfae055c539862061943
divyam <divyamlather509740@gmail.com>
2023-04-25 23:03:25 -0700

    used let and typeof

---
+++ 004-let-typeof/index.js
@@ -0,0 +1,11 @@
+let dogName = "doggy";
+let dogAge = 2;
+let bestDog = true;
+
+console.log(dogName);
+console.log(dogAge);
+console.log(bestDog);
+
+console.log(typeof dogName);
+console.log(typeof dogAge);
+console.log(typeof bestDog);
