3302cdbcc9b323a21ac74ed441fbc78d2fa73465
divyam <divyamlather509740@gmail.com>
2023-04-27 21:52:02 -0700

    my functions

--- 006-functions/main.js
+++ 006-functions/main.js
@@ -9,3 +9,18 @@ function doMath(a, b) {
 
 const result = doMath(10, 4)
 console.log(`this result was returned from the function ${result}`)
+
+function doAdd(a, b) {
+    let result = a + b
+    console.log(`inside doAdd, result = ${result}`)
+}
+
+doAdd(20, 13)
+
+function doSub(a, b) {
+    let result = a - b
+    return result
+}
+
+const result2 = doSub(55, 11)
+console.log(`this result was returned from the function doSub ${result2}`)
