19032b27eee528ea88aa95b0cb811b1f5dcca47d
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-03 12:54:02 +0530

    writing the mileage function

--- 007-more-functions/README.md
+++ 007-more-functions/README.md
@@ -1,1 +1,3 @@
 # 007 More Functions
+
+Yes I'm using devtools, for looking at console.log and stepping over code in sources panel
--- 007-more-functions/main.js
+++ 007-more-functions/main.js
@@ -1,9 +1,11 @@
 const updateDOM = (input) => {
-    const divEl = document.querySelector("#output")
-    const p = document.createElement("p")
-    p.textContent = input
-    divEl.appendChild(p)
-  }
-  
-  const name = 'divyam'
-  updateDOM(`My name is ${name}`)
+  const divEl = document.querySelector("#output")
+  const p = document.createElement("p")
+  p.textContent = input
+  divEl.appendChild(p)
+}
+
+const mileage = (miles, gallons, price = 3.79) => miles/gallons * price
+
+
+updateDOM(mileage(300, 10, 5.40))
