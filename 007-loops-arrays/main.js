25f5f9b902c62a3cf8fb8a54210d803dd2f98e80
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-07 00:19:26 +0530

    my code for average trip cost

--- 007-loops-arrays/main.js
+++ 007-loops-arrays/main.js
@@ -8,7 +8,7 @@ const updateDOM = (input) => {
   divEl.appendChild(p)
 }
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
+const trackMPGAndCost = (miles, gallons, price = 3.79) => {
   const MPG = Math.round(miles/gallons) 
   const tripCost = Math.round(MPG * price)
   myMPG.push(MPG)
@@ -16,19 +16,24 @@ const trackMPGCost = (miles, gallons, price = 3.79) => {
   updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
 }
 
-const calculateMPGAvg = () => {
+const calculateMPGAndTripCostAvg = () => {
   let totalMPG = 0
+  let totalCost = 0
   for(let i = 0; i < myMPG.length; i++) {
     totalMPG = totalMPG + myMPG[i]
+    totalCost = totalCost + myTripCost[i]
   }
-  const avgMPG = totalMPG / myMPG.length 
+  const avgMPG = Math.round(totalMPG / myMPG.length) 
   updateDOM(`Average MPG is ${avgMPG}`)
+
+  const avgCost = Math.round(totalCost / myTripCost.length)
+  updateDOM(`Average Trip Cost is ${avgCost}`)
 }
 
-trackMPGCost(360, 15, 5.40)
-trackMPGCost(320, 12, 5)
-trackMPGCost(100, 7, 4.40)
-trackMPGCost(600, 24, 5.70)
-trackMPGCost(50, 2, 3.40)
-trackMPGCost(320, 12, 5)
-calculateMPGAvg()
+trackMPGAndCost(360, 15, 5.40)
+trackMPGAndCost(320, 12, 5)
+trackMPGAndCost(100, 7, 4.40)
+trackMPGAndCost(600, 24, 5.70)
+trackMPGAndCost(50, 2, 3.40)
+trackMPGAndCost(320, 12, 5)
+calculateMPGAndTripCostAvg()
