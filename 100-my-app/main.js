1f730ce44f7351031434a12291097c325b41059d
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-15 23:57:38 -0700

    my form input

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,4 +1,12 @@
 const FORM = document.getElementById("form-input")
+const ERR = document.getElementById("error")
+
+function updateDOM(input, id) {
+  const divEl = document.getElementById(id);
+  const p = document.createElement("p");
+  p.textContent = input;
+  divEl.appendChild(p);
+}
 
 function shouldIEat(amIHungry, hungerLevel, foodName) {
   let obj = {
@@ -18,9 +26,16 @@ function shouldIEat(amIHungry, hungerLevel, foodName) {
   return obj
 }
 
-console.log(shouldIEat(false, 80, "pizza"))
-console.log(shouldIEat(true, 65, "bread"))
-console.log(shouldIEat(false, 1, "rice"))
+function isFormValid(amIHungry, hungerLevel, foodName) {
+  const errMsg = []
+  if(hungerLevel < 1 || hungerLevel > 100 ) {
+    errMsg.push("hunger level must be between 1-100")
+    ERR.textContent = errMsg
+    return false
+  }
+  ERR.textContent = ""
+  return true
+}
 
 FORM.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -28,6 +43,12 @@ FORM.addEventListener('submit', (e) => {
   const hungerLevel = parseInt(e.target.hungerLevel.value)
   const foodName = e.target.foodName.value
 
-  console.log("form submit ", shouldIEat(amIHungry, hungerLevel, foodName))
+  if(isFormValid(amIHungry, hungerLevel, foodName)) {
+    const result = shouldIEat(amIHungry, hungerLevel, foodName)
+    updateDOM(`amIHungry=${result.amIHungry}, hungerLevel=${result.hungerLevel}, foodName=${result.foodName}, decision=${result.decision}`, "output")
+  } else {
+    alert()
+  }
+
   FORM.reset()
 })
