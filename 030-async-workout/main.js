c355b9ed28dcefcfa6c771ca1bb2cb931ef672ec
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 13:37:36 -0700

    my code

--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,10 +1,33 @@
 const formEl = document.getElementById("form")
-const output = document.getElementById("output")
+
+function updateDOM(input, id) {
+  const divEl = document.getElementById(id);
+  const p = document.createElement("p");
+  p.textContent = input;
+  divEl.appendChild(p);
+}
+
+function startWorkout(time, initialUpdate, finalUpdate) {
+  initialUpdate()
+  setTimeout(() => {
+    finalUpdate()
+  }, time*60*1000)
+}
+
+function acceptWorkout(type, reps, time) {
+  startWorkout(time, () => {
+    updateDOM(`type=${type}, reps=${reps} time=${time}`, 'output')
+  },
+  () => {
+    updateDOM('done', 'output') 
+  })
+}
 
 formEl.addEventListener("submit", function(e) {
   e.preventDefault()
   const type = e.target.type.value
   const reps = parseFloat(e.target.reps.value)
   const time = parseFloat(e.target.time.value)
+  acceptWorkout(type, reps, time)
   formEl.reset()
 })
