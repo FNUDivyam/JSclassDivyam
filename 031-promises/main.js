89f279f9f3cbb3692f6d8a09f01eb5f1c0de7dcd
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 14:37:24 -0700

    real world example

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -67,3 +67,19 @@ function getWeather() {
     .catch(onError)
     .finally(onFinally)
   
+  
+  function fetchData() {
+    return new Promise(function(resolve, reject) {
+      fetch("https://api.weather.gov/gridpoints/OKX/35,35/forecast")
+        .then(response => response.json())
+        .then(data => resolve(data.properties.periods[1].shortForecast))
+    })
+  }
+  
+  function displayData(weather) {
+    console.log(weather)
+  }
+  
+  fetchData()
+    .then(displayData)
+    .catch(onError)
