bc6d9fbeb6b6ca0f17301bdb63460e5b44c12d74
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-15 19:40:48 -0700

    my function

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,11 +1,21 @@
-let amIHungry = true
-let hungerLevel = 80
-let foodName = "pizza"
-
-if(foodName === "pizza") {
-  console.log("I'll eat a slice")
-} else if(amIHungry && hungerLevel > 60) {
-  console.log("time to eat")
-} else {
-  console.log("not eating now")
-}
+function shouldIEat(amIHungry, hungerLevel, foodName) {
+    let obj = {
+      amIHungry: amIHungry,
+      hungerLevel: hungerLevel,
+      foodName: foodName
+    }
+  
+    if(foodName === "pizza") {
+      obj.decision = "I'll eat a slice"
+    } else if(amIHungry && hungerLevel > 60) {
+      obj.decision = "time to eat"
+    } else {
+      obj.decision = "not eating now"
+    }
+  
+    return obj
+  }
+  
+  console.log(shouldIEat(false, 80, "pizza"))
+  console.log(shouldIEat(true, 65, "bread"))
+  console.log(shouldIEat(false, 1, "rice"))
