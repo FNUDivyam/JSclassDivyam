966b6f43c45c78e019c94dec1212512ba79d9146
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 11:29:12 -0700

    student subclass

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -5,3 +5,6 @@ yes, I can see the difference, one main thing I can see is
 previously there was only one object 
 
 but now we can create new objects with new Person()
+
+## does this student subclass make more sense and prototype chain makes sense?
+yes, the first occurence of the function in the prototpe chain is called
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,14 +1,25 @@
 class Person {
-    constructor(firstName, lastName, address) {
-      this.firstName = firstName
-      this.lastName = lastName
-      this.address = address
-    }
-  
-    showStatus() {
-      console.log(`${this.firstName} lives at ${this.address}`)
-    }
+  constructor(firstName, lastName, address) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.address = address
   }
-  
-  const divyam = new Person('Divyam', 'Divyam', '3, Earth, Milky Way Galaxy')
-  const jane = new Person('Jane', 'Doe', '12, My Street')
+
+  showStatus() {
+    console.log(`${this.firstName} lives at ${this.address}`)
+  }
+}
+
+class Student extends Person {
+  constructor(firstName, lastName, address, classList) {
+    super(firstName, lastName, address)
+    this.classList = classList
+  }
+
+  showStatus() {
+    console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`)
+  }
+}
+
+const divyam = new Student('Divyam', 'Divyam', '3, Earth, Milky Way Galaxy', ["Eng1A", "CIT93"])
+const jane = new Person('Jane', 'Doe', '12, My Street')
