ef2457b2f45200442cefbee495df5bf45831a6c2
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-15 19:57:17 -0700

    my form

--- 100-my-app/main.js
+++ 100-my-app/main.js
@@ -1,21 +1,33 @@
+const FORM = document.getElementById("form-input")
+
 function shouldIEat(amIHungry, hungerLevel, foodName) {
-    let obj = {
-      amIHungry: amIHungry,
-      hungerLevel: hungerLevel,
-      foodName: foodName
-    }
-  
-    if(foodName === "pizza") {
-      obj.decision = "I'll eat a slice"
-    } else if(amIHungry && hungerLevel > 60) {
-      obj.decision = "time to eat"
-    } else {
-      obj.decision = "not eating now"
-    }
-  
-    return obj
+  let obj = {
+    amIHungry: amIHungry,
+    hungerLevel: hungerLevel,
+    foodName: foodName
   }
-  
-  console.log(shouldIEat(false, 80, "pizza"))
-  console.log(shouldIEat(true, 65, "bread"))
-  console.log(shouldIEat(false, 1, "rice"))
+
+  if(foodName === "pizza") {
+    obj.decision = "I'll eat a slice"
+  } else if(amIHungry && hungerLevel > 60) {
+    obj.decision = "time to eat"
+  } else {
+    obj.decision = "not eating now"
+  }
+
+  return obj
+}
+
+console.log(shouldIEat(false, 80, "pizza"))
+console.log(shouldIEat(true, 65, "bread"))
+console.log(shouldIEat(false, 1, "rice"))
+
+FORM.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const amIHungry = e.target.amIHungry.checked
+  const hungerLevel = parseInt(e.target.hungerLevel.value)
+  const foodName = e.target.foodName.value
+
+  console.log("form submit ", shouldIEat(amIHungry, hungerLevel, foodName))
+  FORM.reset()
+})
