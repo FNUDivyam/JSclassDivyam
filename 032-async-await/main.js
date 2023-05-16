be12cba225cec889dbfc212b6a22090a70c10f2a
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 16:48:59 -0700

    try/catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,7 +1,26 @@
-async function start() {
-    const response = await fetch("https://api.weather.gov/gridpoints/HNX/53,100/forecast")
-    const result = await response.json()
-    console.log(result.properties.periods[0].shortForecast)
-}
+// async function start() {
+//   const response = await fetch("https://api.weather.gov/gridpoints/HNX/53,100/forecast")
+//   const result = await response.json()
+//   console.log(result.properties.periods[0].shortForecast)
+// }
 
-start()
+// start()
+
+function getData() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        reject("something went wrong!")
+      }, 1)
+    })
+  }
+  
+  async function start() {
+    try {
+      const result = await getData()
+      console.log(`result: ${result}`)
+    } catch(error) {
+      console.log(`ERROR: ${error}`)
+    }
+  }
+  
+  start()
