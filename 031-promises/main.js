a6458c4c73d6c445f8e28a852a1256f1ec8210b6
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 14:24:57 -0700

    intro to promises

---
+++ 031-promises/main.js
@@ -0,0 +1,39 @@
+function getWeather() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        resolve("Sunny")
+      }, 100)
+    })
+  }
+  
+  function getWeatherIcon(weather) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        switch(weather) {
+          case 'Sunny':
+            resolve("☀️")
+            break
+          case 'Cloudy':
+            resolve("🌥️")
+            break
+          case 'Rainy':
+            resolve("🌦️")
+            break
+          default:
+          reject("NO ICON FOUND")
+        }
+      }, 100)
+    })
+  }
+  
+  function onSuccess(data) {
+    console.log(`Success ${data}`)
+  }
+  
+  function onError(error) {
+    console.log(`Error: ${error}`)
+  }
+  
+  getWeather()
+    .then(getWeatherIcon)
+    .then(onSuccess, onError)
