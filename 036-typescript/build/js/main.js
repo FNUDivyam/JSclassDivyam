7fd3e38a638475ef0ec23e43db01a76c2a00fca0
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 20:29:45 -0700

    finish basic types

--- 036-typescript/build/js/main.js
+++ 036-typescript/build/js/main.js
@@ -6,3 +6,10 @@ let album;
 myName = 'divyam';
 meaningOfLife = 42;
 isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -3,6 +3,17 @@ let meaningOfLife: number
 let isLoading: boolean
 let album: any
 
+
 myName = 'divyam'
 meaningOfLife = 42
 isLoading = true
+album  = 5150
+
+const sum = (a: number, b: number) => {
+  return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
