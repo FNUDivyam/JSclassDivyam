7475640612a7de8d2b076d20eb3778a8fed25de2
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 17:08:50 -0700

    updated code

--- 034-photo-album/main.js
+++ 034-photo-album/main.js
@@ -2,20 +2,27 @@ const OUTPUT = document.getElementById("output")
 
 function renderPhoto(photo) {
   const img = document.createElement("img")
-  img.setAttribute("src", photo.url)
+  img.setAttribute("src", photo.thumbnailUrl)
   OUTPUT.appendChild(img)
 }
 
+async function getPhotos() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/photos")
+    return await response.json()
+  } catch (error) {
+    console.log(`Error ${error}`)
+  }
+}
 
 async function start() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/photos")
-  const result = await response.json()
-  for(let i=0; i<21;i++) {
-    try {
-      renderPhoto(result[i])
-    } catch(error) {
-      console.log(`Error ${error}`)
+  try {
+    const photos = await getPhotos()
+    for(let i=0; i<photos.length;i++) {
+      renderPhoto(photos[i])
     }
+  } catch (error) {
+    console.log(`Error ${error}`)
   }
 }
 
