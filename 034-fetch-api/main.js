21f7ade10c5a89d3cc7c85764640f147ee38c73a
FNUDivyam <97506726+FNUDivyam@users.noreply.github.com>
2023-05-16 18:51:34 -0700

    fetch header object

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,10 +1,23 @@
-const api_token = ''
-const url = `https://api.thenewsapi.com/v1/news/top?api_token=${api_token}`
+const url = `https://api.spotify.com/v1/artists/0k17h0D3J5VfsdmQ1ZtE9`
+const auth_token = ''
 
 async function getData() {
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log(data)
+  try {
+    const request = new Request(url, {
+      headers: {
+        'Authorization': `Bearer ${auth_token}`
+      }
+    })
+    const response = await fetch(request)
+    const data = await response.json()
+    if(response.status == 200) {
+      console.log("Success", data)
+    } else {
+      console.log("Server Error", data)
+    }
+  } catch(error) {
+    console.log('Fetch Error', error)
+  }
 }
 
 getData()
